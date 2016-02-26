89f87fff4e625347f3fb6401ebbd4bd241b128af
Frank Mielke <mielke@de.ibm.com>
2016-02-26 19:14:19 +0100

    Video Chat card

--- modules/Maintenance/Maintenance.jsx
+++ modules/Maintenance/Maintenance.jsx
@@ -1,5 +1,6 @@
 var Maintenance = {};
 Maintenance.Maintenance = require('./cards/Maintenance.jsx');
+Maintenance.VideoChat = require('./cards/VideoChat.jsx');
 Maintenance.MaintenanceStore = require('./stores/MaintenanceStore.js');
 
 module.exports = Maintenance;
