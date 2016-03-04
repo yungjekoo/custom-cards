fbff2ec90c8e622cb895d13a1190b48212208dee
Frank Mielke <mielke@de.ibm.com>
2016-03-04 01:35:18 +0100

    Fixed merge

--- modules/Modules.jsx
+++ modules/Modules.jsx
@@ -1,18 +1,12 @@
 var Modules = {};
 <<<<<<< HEAD
-<<<<<<< HEAD
 Modules.HelloWorld = require('./HelloWorld/HelloWorld.jsx');
 =======
-=======
->>>>>>> 8f970781483479696b9ad1a17e61a7dd2832d4f6
 Modules.Maintenance = require('./Maintenance/Maintenance.jsx');
 Modules.ElevatorDemo = require('./ElevatorDemo/ElevatorDemo.jsx');
 Modules.WeatherService = require('./WeatherService/WeatherService.jsx');
 Modules.Runtastic = require('./Runtastic/Runtastic.jsx');
 Modules.ToggleGroup = require('./ToggleGroup/ToggleGroup.jsx');
 
-<<<<<<< HEAD
->>>>>>> 8f970781483479696b9ad1a17e61a7dd2832d4f6
-=======
 >>>>>>> 8f970781483479696b9ad1a17e61a7dd2832d4f6
 module.exports = Modules;
