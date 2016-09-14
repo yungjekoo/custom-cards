9214d0e488d2295e2bf838fbbf8be468a019d97a
Frank Mielke <mielke@de.ibm.com>
2016-09-14 11:30:56 +0200

    SSL support, copyright, removal of internal stuff

--- modules/EmptyCard/cards/EmptyCard.jsx
+++ modules/EmptyCard/cards/EmptyCard.jsx
@@ -1,5 +1,22 @@
+/*****************************************************************************
+Copyright (c) 2016 IBM Corporation and other Contributors.
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+
+
+Contributors:
+
+Frank Leo Mielke - Initial Contribution
+*****************************************************************************/
 var React = require('react');
-var IoTFCommon = require('IoTFCommon');
 
 var RPT = React.PropTypes;
 
@@ -25,7 +42,7 @@ var EmptyCard = React.createClass({
   render: function() {
     return (
 			<div style={styles.container}>
-				Hello World!
+				This is an empty card!
 			</div>
 		)
   }
--- modules/HelloWorld/customization/HelloWorldProperties.jsx
+++ modules/HelloWorld/customization/HelloWorldProperties.jsx
@@ -1,30 +1,50 @@
+/*****************************************************************************
+Copyright (c) 2016 IBM Corporation and other Contributors.
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+
+
+Contributors:
+
+Frank Leo Mielke - Initial Contribution
+*****************************************************************************/
 var React = require('react');
-var IoTFCommon = require('IoTFCommon');
 var RPT = React.PropTypes;
-var InputField = IoTFCommon.InputField;
-var Label = IoTFCommon.Label;
-var ColorSelection = IoTFCommon.ColorSelection;
 
 var styles = {
   container: {
-
   },
-  table: {
-    width: "100%"
+  preview: {
+    width: "300px",
+    height: "100px",
+    marginTop: "20px"
+  },
+  button: {
+    marginRight: "10px"
   }
 }
 
 var HelloWorldProperties = React.createClass({
 
   propTypes: {
-    helloColor: RPT.string,
     nls: RPT.object,
     style: RPT.object,
-    theme: RPT.object.isRequired
+    theme: RPT.object.isRequired,
+    // This is the card specific parameter. We receive the current value as prop.
+    helloColor: RPT.string
   },
 
   getDefaultProps: function() {
     return {
+      // The default value
       helloColor: 0
     };
   },
@@ -41,39 +61,31 @@ var HelloWorldProperties = React.createClass({
     })
   },
 
-  onUpdate: function(state) {
-    var state = Object.assign({},this.state, state);
-    this.setState(state);
-  },
-
   render: function() {
     var self = this;
 
     if (this.state) {
       return (
         <div style={Object.assign({}, styles.container, this.props.style)}>
-                <table style={styles.table}>
-                  <tbody>
-                    <tr>
-                      <td colSpan="2">
-			            <Label label={this.props.nls.resolve('ColorScheme')} theme={this.props.theme}>
-			                <ColorSelection theme={this.props.theme} onChange={this.onColorChanged} initialSelection={this.state.helloColor}></ColorSelection>
-			            </Label>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+          <h2>
+            {this.props.nls.resolve('ColorScheme')}
+          </h2>
+          <button style={styles.button} onClick={function() {self.onColorChanged(0)}}>Purple</button>
+          <button style={styles.button} onClick={function() {self.onColorChanged(1)}}>Red</button>
+          <button style={styles.button} onClick={function() {self.onColorChanged(2)}}>Green</button>
+          <button style={styles.button} onClick={function() {self.onColorChanged(3)}}>Blue</button>
+          <button style={styles.button} onClick={function() {self.onColorChanged(4)}}>Teal</button>
+          <div style={Object.assign({}, styles.preview, {backgroundColor: this.props.theme.schemes[this.state.helloColor].normal})}/>
         </div>
       );
   } else {
     return (
         <div>
+          Loading...
         </div>
     )
   }
 }
 });
-
-
 
 module.exports = HelloWorldProperties;
